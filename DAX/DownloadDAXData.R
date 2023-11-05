853bb866885af18fd8ebe4a9db4155326022613d
EliasKng <elias.koenig@student.kit.edu>
2023-11-05 11:51:30 +0100

    Redefine as function

--- DAX/DownloadDAXData.R
+++ DAX/DownloadDAXData.R
@@ -1,10 +1,11 @@
 library(tidyquant)
 setwd("/Users/elias/Desktop/PTSFC/DAX/")
 
-options("getSymbols.warning4.0"=FALSE)
-options("getSymbols.yahoo.warning"=FALSE)
-# Downloading Apple price using quantmod
-
-getSymbols("^GDAXI", from = '1987-12-30',warnings = FALSE,
-           auto.assign = TRUE)
-
+get_dax_data <- function() {
+  options("getSymbols.warning4.0"=FALSE)
+  options("getSymbols.yahoo.warning"=FALSE)
+  # Downloading Apple price using quantmod
+  getSymbols("^GDAXI", from = '1987-12-30',warnings = FALSE,
+              auto.assign = TRUE)
+  return (GDAXI)
+}
