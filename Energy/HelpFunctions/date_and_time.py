37ccf5161c0dfc1d48cc5f3652e376b2d0c8088d
EliasKng <elias.koenig@student.kit.edu>
2023-11-13 08:41:47 +0100

    Move split_time function to helper functions

--- Energy/HelpFunctions/date_and_time.py
+++ Energy/HelpFunctions/date_and_time.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import pandas as pd
 
 def most_recent_thursday(df):
     today = df.index.max()
@@ -10,3 +11,13 @@ def most_recent_thursday(df):
     most_recent_thursday = recent_thursday.replace(hour=0, minute=0, second=0, microsecond=0)
 
     return most_recent_thursday
+
+
+def split_time(df, num_years=0, num_months=0, num_weeks=0, num_days=0, num_hours=0):
+    # date_to_keep = df.index.max() - num_years - num_months - num_days
+    split_date = df.index.max() - pd.DateOffset(years=num_years, months=num_months, weeks=num_weeks, days=num_days,
+                                                hours=num_hours)
+    df_b = df.loc[df.index <= split_date]
+    df_a = df.loc[df.index > split_date]
+
+    return df_b, df_a
