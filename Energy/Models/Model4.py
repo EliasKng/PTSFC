06de63302470bf36b89ccfaed69066e7ff6c31c6
EliasKng <elias.koenig@student.kit.edu>
2023-11-25 20:19:10 +0100

    Energy: Model4: Add

---
+++ Energy/Models/Model4.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import statsmodels.api as sm
+
+from Energy.HelpFunctions.dummy_mapping import get_winter_dummy, get_consumption_time_mapping, get_day_mapping, \
+    get_holiday_dummy
+
+
+def model4(energyconsumption):
+    energyconsumption = energyconsumption.rename(columns={"gesamt": "energy_consumption"})
+
+    energyconsumption = add_dummies(energyconsumption)
+
+    y_ec = energyconsumption['energy_consumption']
+    X_ec = energyconsumption.drop(columns=['energy_consumption'])
+
+
+    # add constant for the intercept term
+    X_ec = sm.add_constant(X_ec)
+
+    # Calculate Forecasts
+    # create new dataframe containing future date_times and indepentent variables
+
+    last_ts = energyconsumption.index[-1]
+    horizon = pd.date_range(start=last_ts + pd.DateOffset(
+        hours=1), periods=200, freq='H')
+
+    energy_forecast = pd.DataFrame({'date_time': horizon})
+    energy_forecast.set_index('date_time', inplace=True)
+
+    energy_forecast = add_dummies(energy_forecast)
+
+    # Point forecasts
+    X_fc = energy_forecast
+    X_fc = sm.add_constant(X_fc, has_constant='add')
+
+    # Quantile Regression
+    quantiles = [0.025, 0.25, 0.5, 0.75, 0.975]
+
+    model_qr = sm.QuantReg(y_ec, X_ec)
+
+    for q in quantiles:
+        model_temp = model_qr.fit(q=q)
+        print(X_fc)
+
+        # Calculate forecasts for X_fc using the fitted model for the current quantile
+        forecast_temp = model_temp.predict(X_fc)
+
+        # Add the forecasts to the energy_forecast DataFrame with a label like 'forecast025'
+        energy_forecast[f'q{q}'] = forecast_temp
+
+    indexes = [36, 40, 44, 60, 64, 68]
+
+    forecasting_results = energy_forecast.iloc[indexes,
+                          energy_forecast.columns.get_loc('q0.025'):energy_forecast.columns.get_loc('q0.975') + 1]
+
+    forecasting_results = forecasting_results.reset_index(drop=False)
+    forecasting_results = forecasting_results.rename(columns={"date_time": "forecast_date"})
+    forecasting_results['horizon'] = ['36 hour', '40 hour', '44 hour', '60 hour', '64 hour', '68 hour']
+    forecasting_results['target'] = ["energy" for _ in range(6)]
+
+    return forecasting_results
+
+
+def add_dummies(df):
+    df = get_winter_dummy(df)
+    df = get_consumption_time_mapping(df)
+    df = get_day_mapping(df)
+    df = get_holiday_dummy(df)
+
+    return df
