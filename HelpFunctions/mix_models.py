e0955eee9babd6f1ea14791bb7c7bb0e569d1b34
EliasKng <elias.koenig@student.kit.edu>
2023-11-29 12:43:57 +0100

    Create submission file (And fix mix_models)

--- HelpFunctions/mix_models.py
+++ HelpFunctions/mix_models.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def mix_models(models, weights, df):
+def mix_models(models, weights, df, target):
     # Check that all weights sum to 1, otherwise normalize them
     total_weight = sum(weights)
     if total_weight != 1:
@@ -14,36 +14,47 @@ def mix_models(models, weights, df):
         mixed_forecasts += f * w
     mixed_forecasts['forecast_date'] = forecast_dates
     mixed_forecasts['horizon'] = horizons
-    mixed_forecasts = mixed_forecasts[['forecast_date', 'horizon', 'q0.025', 'q0.25', 'q0.5', 'q0.75', 'q0.975']]
+
+    mixed_forecasts['target'] = [target for _ in range(len(mixed_forecasts))]
+    mixed_forecasts = mixed_forecasts[
+        ['forecast_date', 'target', 'horizon', 'q0.025', 'q0.25', 'q0.5', 'q0.75', 'q0.975']]
+
+
     return mixed_forecasts
 
 
 # Now weights should contain a list of lists of weights per horizon e.g., for five horizons with two models:
 # [ [ 1, 0.5, 0.5, 0.5, 0.5], [0, 0.5, 0.5, 0.5, 0.5]]
-def mix_models_per_horizon(models, weights, df):
+def mix_models_per_horizon(models, weights, df, target, offset_horizons = 0):
     weights, did_normalize = _normalize_columns(np.array(weights))
     if did_normalize:
         print("Used normalized weights instead:")
         print(weights)
 
-    model_forecasts, forecast_dates, horizons = _run_models(models, df)
+    model_forecasts, forecast_dates, horizons = _run_models(models, df, offset_horizons)
 
     mixed_forecasts = np.zeros(model_forecasts[0].shape)
     for F, w in zip(model_forecasts, weights):
         mixed_forecasts += F * w[:, np.newaxis]
     mixed_forecasts['forecast_date'] = forecast_dates
     mixed_forecasts['horizon'] = horizons
-    mixed_forecasts = mixed_forecasts[['forecast_date', 'horizon', 'q0.025', 'q0.25', 'q0.5', 'q0.75', 'q0.975']]
+
+    mixed_forecasts['target'] = [target for _ in range(len(mixed_forecasts))]
+    mixed_forecasts = mixed_forecasts[
+        ['forecast_date', 'target', 'horizon', 'q0.025', 'q0.25', 'q0.5', 'q0.75', 'q0.975']]
 
     return mixed_forecasts
 
 
-def _run_models(models, df):
+def _run_models(models, df, offset_horizons = 0):
     forecast_dates = []
     model_forecasts = []
     horizons = []
     for m in models:
-        forecast = m(df)
+        if offset_horizons == 0:
+            forecast = m(df)
+        else:
+            forecast = m(df, offset_horizons=offset_horizons)
         model_forecasts.append(forecast.loc[:, ['q0.025', 'q0.25', 'q0.5', 'q0.75', 'q0.975']])
         forecast_dates = forecast['forecast_date']
         horizons = forecast['horizon']
